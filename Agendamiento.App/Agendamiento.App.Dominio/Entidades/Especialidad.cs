b3ffbde565de67e87d5299fd32f3b7b4a2a68027
DavidV87 <ibore03@gmail.com>
2021-10-14 03:18:08 -0500

    Creacion Clase Especialidad (Lista especialidades)

---
+++ Agendamiento.App/Agendamiento.App.Dominio/Entidades/Especialidad.cs
@@ -0,0 +1,27 @@
+using System;
+namespace Agendamiento.App.Dominio
+{
+    /// <summary>
+    /// Lista de especialidades
+    /// </summary>
+    public enum Especialidad
+    {
+        Alergología,
+        Anestesiología,
+        Cardiología,
+        Endocrinología,
+        Geriatría,
+        Hematología,
+        Medicina_intensiva,
+        Medicina_interna,
+        Nefrología,
+        Neumología,
+        Neurología,
+        Neurofisiología,
+        Oncología,
+        Pediatría,
+        Psiquiatría,
+        Rehabilitación,
+        Reumatología,
+    }
+}
