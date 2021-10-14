968b55cf99daffefc02c82fc0e861cd959f64226
DavidV87 <ibore03@gmail.com>
2021-10-14 08:47:48 -0500

    Se guardaron registros de pureba en BD

--- Agendamiento.App/Agendamiento.App.Consola/Program.cs
+++ Agendamiento.App/Agendamiento.App.Consola/Program.cs
@@ -1,13 +1,30 @@
 ﻿using System;
 using Agendamiento.App.Dominio;
+using Agendamiento.App.Persistencia;
 
 namespace Agendamiento.App.Consola
 {
     class Program
     {
+        private static IRepositorioPaciente _repoPaciente = new RepositorioPaciente(new Persistencia.AppContext());
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            AddPaciente();
         }
+        private static void AddPaciente()
+        {
+            var paciente = new Paciente
+            {
+                Nombre="Elkin",
+                Apellido ="Valencia",
+                NumeroTelefono="3184016384",
+                Genero=Genero.Masculino,
+                Direccion="cra 27B 124 90",
+                Ciudad=Ciudad.Armenia
+            };
+            _repoPaciente.AddPaciente(paciente);
+
+        }   
     }
 }
