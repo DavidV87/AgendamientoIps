7303288f6f90490c4ba02c52d2fef32014d95083
DavidV87 <ibore03@gmail.com>
2021-10-14 04:15:25 -0500

    Creacion Consola Instal EF Design Ref Persitencia

---
+++ Agendamiento.App/Agendamiento.App.Consola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Agendamiento.App.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
