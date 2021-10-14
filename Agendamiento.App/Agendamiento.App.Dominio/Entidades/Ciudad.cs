163b274f7d720974bc84fe0d181177abfa4c8e30
DavidV87 <ibore03@gmail.com>
2021-10-14 03:07:29 -0500

    Creacion clase Ciudad (lista de ciudades de Ips)

---
+++ Agendamiento.App/Agendamiento.App.Dominio/Entidades/Ciudad.cs
@@ -0,0 +1,14 @@
+using System;
+namespace Agendamiento.App.Dominio
+{
+    /// <summary>
+    /// Lista de ciudades
+    /// </summary>
+    public enum Ciudad
+    {
+        Pereira,
+        Manizales,
+        Armenia,
+        Medellin,
+    }
+}
