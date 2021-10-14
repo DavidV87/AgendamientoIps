f017d0d8bf4c03b0103b743f457617392d093ea7
DavidV87 <ibore03@gmail.com>
2021-10-14 03:04:40 -0500

    Creacion de clase Genero (lista de generos)

---
+++ Agendamiento.App/Agendamiento.App.Dominio/Entidades/Genero.cs
@@ -0,0 +1,17 @@
+using System;
+namespace Agendamiento.App.Dominio
+{
+    /// <summary>
+    /// Lista de generos
+    /// </summary>
+    public enum Genero
+    {
+        Masculino,
+        Femenino,
+        Gay,
+        Bisexual,
+        Intersexual,
+        Pansexual,
+        Transexual
+    }
+}
