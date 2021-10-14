309cefaf4aeb689be7881573a6a5984d456aebfa
DavidV87 <ibore03@gmail.com>
2021-10-14 02:25:12 -0500

    first commit

---
+++ Agendamiento.App/Agendamiento.App.Dominio/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Agendamiento.App.Dominio
+{
+    public class Class1
+    {
+    }
+}
---
+++ Agendamiento.App/Agendamiento.App.Persistencia/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Agendamiento.App.Persistencia
+{
+    public class Class1
+    {
+    }
+}
