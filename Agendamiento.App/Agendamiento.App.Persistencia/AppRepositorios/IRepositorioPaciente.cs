9d8c2897b1c0dd5116121a82bee57e333a58b4c3
DavidV87 <ibore03@gmail.com>
2021-10-14 07:19:20 -0500

    Creacion Interfaz Repo Paciente CRUD basico

---
+++ Agendamiento.App/Agendamiento.App.Persistencia/AppRepositorios/IRepositorioPaciente.cs
@@ -0,0 +1,18 @@
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Agendamiento.App.Dominio;
+
+namespace Agendamiento.App.Persistencia
+{
+    public interface IRepositorioPaciente
+    {
+        IEnumerable<Paciente> GetAllPacientes();
+        Paciente AddPaciente (Paciente paciente);
+        Paciente UpdatePaciente (Paciente paciente);
+        Paciente GetPaciente(int idPaciente);
+        void DeletePaciente (int idPaciente);
+    }
+}
