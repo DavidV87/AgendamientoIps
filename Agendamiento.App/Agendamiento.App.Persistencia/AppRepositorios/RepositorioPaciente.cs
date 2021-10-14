2893a7147e20e31dc50ede80913af03b958750d6
DavidV87 <ibore03@gmail.com>
2021-10-14 07:37:05 -0500

    Clase RepositorioPacientes impl interf sin error

---
+++ Agendamiento.App/Agendamiento.App.Persistencia/AppRepositorios/RepositorioPaciente.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Agendamiento.App.Dominio;
+using Microsoft.EntityFrameworkCore;
+
+namespace Agendamiento.App.Persistencia
+{
+
+    public class RepositorioPaciente : IRepositorioPaciente
+    {
+        /// <summary>
+        /// Referencia al contexto de Paciente
+        /// </summary>
+        private readonly AppContext _appContext;
+        /// <summary>
+        /// Metodo Constructor Utiiza 
+        /// Inyeccion de dependencias para indicar el contexto a utilizar
+        /// </summary>
+        /// <param name="appContext"></param>//
+        public RepositorioPaciente(AppContext appContext)
+        {
+            _appContext = appContext;
+        }
+
+
+        public Paciente AddPaciente(Paciente paciente)
+        {
+            var pacienteAdicionado = _appContext.Pacientes.Add(paciente);
+            _appContext.SaveChanges();
+            return pacienteAdicionado.Entity;
+
+        }
+
+        public void DeletePaciente(int idPaciente)
+        {
+            var pacienteEncontrado = _appContext.Pacientes.FirstOrDefault(p => p.Id == idPaciente);
+            if (pacienteEncontrado == null)
+                return;
+            _appContext.Pacientes.Remove(pacienteEncontrado);
+            _appContext.SaveChanges();
+        }
+
+       public IEnumerable<Paciente> GetAllPacientes()
+        {
+            return _appContext.Pacientes;
+        }
+        
+        public Paciente GetPaciente(int idPaciente)
+        {
+            return _appContext.Pacientes.FirstOrDefault(p => p.Id == idPaciente);
+        }
+
+        public Paciente UpdatePaciente(Paciente paciente)
+        {
+            var pacienteEncontrado = _appContext.Pacientes.FirstOrDefault(p => p.Id == paciente.Id);
+            if (pacienteEncontrado != null)
+            {
+                pacienteEncontrado.Nombre=paciente.Nombre;
+                pacienteEncontrado.Apellido=paciente.Apellido;
+                pacienteEncontrado.NumeroTelefono=paciente.NumeroTelefono;
+                pacienteEncontrado.Direccion=paciente.Direccion;
+                pacienteEncontrado.Ciudad=paciente.Ciudad;
+                _appContext.SaveChanges();
+
+            }
+            return pacienteEncontrado;
+        }
+
+        
+    }
+}
