3b272903cdc69778b9b14aaf232bc37c97a01c60
DavidV87 <ibore03@gmail.com>
2021-10-16 11:03:17 -0500

    Vista Crear sedes ips

---
+++ Agendamiento.App/Agendamiento.App.Frontend/Pages/SedesIps/CrearSedes.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Agendamiento.App.Frontend.Pages.SedesIps
+{
+    public class CrearSedesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
